865e99cc61bd3994eb44144d8db24ce2370f758c
Pavan Kumar C <pavan.challa23@gmail.com>
2018-12-19 12:51:28 +0530

    handled message for empty ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,20 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key = {igKey + i} type= {igKey} />;
-        });
-    });
+        })
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding the ingredients!</p>;
+    }
+
+    console.log(transformedIngredients);
 
     return (
         <div className={classes.Burger}>
